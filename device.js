c6e84ea4d8cc5341f18a7bc6eb9aac73b8fa0bec
jrflat <50605158+jrflat@users.noreply.github.com>
2019-06-04 04:16:17 -0700

    Update device.js

--- device.js
+++ device.js
@@ -3,8 +3,25 @@
 const Tp = require('thingpedia');
 
 const rss_urls = {'nyt' : 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+		              'nytimes' : 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+		              'newyorktimes' : 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
                   'cnn' : 'http://rss.cnn.com/rss/cnn_topstories.rss',
-                  'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml'};
+                  'cnnnews' : 'http://rss.cnn.com/rss/cnn_topstories.rss',
+                  'cablenewsnetwork' : 'http://rss.cnn.com/rss/cnn_topstories.rss',
+                  'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml',
+                  'bbcnews' : 'http://feeds.bbci.co.uk/news/rss.xml',
+                  'britishbroadcastingcorporation' : 'http://feeds.bbci.co.uk/news/rss.xml',
+		              'abc' : 'https://abcnews.go.com/abcnews/topstories',
+		              'abcnews' : 'https://abcnews.go.com/abcnews/topstories',
+		              'americanbroadcastingcompany' : 'https://abcnews.go.com/abcnews/topstories',
+		              'businessinsider' : 'http://feeds2.feedburner.com/businessinsider',
+		              'fortune' : 'http://fortune.com/feed/',
+		              'fortunenews' : 'http://fortune.com/feed/',
+		              'huffington' : 'https://www.huffingtonpost.com/section/front-page/feed',
+		              'huffingtonpost' : 'https://www.huffingtonpost.com/section/front-page/feed',
+		              'mashable' : 'http://feeds.mashable.com/Mashable',
+		              'mashablenews' : 'http://feeds.mashable.com/Mashable'
+};
 
 module.exports = class newsfeedDevice extends Tp.BaseDevice {
     constructor(engine, state) {
@@ -18,15 +35,15 @@ module.exports = class newsfeedDevice extends Tp.BaseDevice {
     splitMultiString(string) {
         console.log('news sources string is ' + string);
         string = string.toString().toLowerCase();
+	      string = string.replace(' the ', '');
         // string can be split by the word comma or actual ','
         string = string.replace(new RegExp(' comma ', 'g'), ',');
+	      string = string.replace(/\s/g, '');
         let sources = string.split(',');
         return sources;
     }
 
     sourcesToURLs(sourceList) {
-        if (sourceList.length == 0) // use New York Times as default
-            return [rss_urls['nyt']];
         let urls = [];
         for (let i = 0; i < sourceList.length; i++) {
             if (!rss_urls.hasOwnProperty(sourceList[i]))
@@ -41,11 +58,11 @@ module.exports = class newsfeedDevice extends Tp.BaseDevice {
         console.log('News sources to pull from are ' + JSON.stringify(sourceList));
         let urls = this.sourcesToURLs(sourceList);
         console.log('urls are ' + JSON.stringify(urls));
-        let news = [];
+        var news = [];
         for (let i = 0; i < urls.length; i++) {
-            let curr = (Tp.Helpers.Rss.get(urls[i])).slice(0, 3);
-            console.log('curr array: ' + JSON.stringify(curr));
-            news.push(curr);
+	          /* Issue here: Not sure how to combine multiple RSS helper calls for one return.
+	          Tried: .push(), .concat(), .push.apply() */
+            news.push.apply(news, Tp.Helpers.Rss.get(urls[i]));
             console.log('news array to return: ' + JSON.stringify(news));
         }
         return news;
